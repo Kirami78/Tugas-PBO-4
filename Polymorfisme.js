9695bb021f3af06990de359e2bb78032e5091b07
Kirami78 <whxxtzl69@gmail.com>
2024-10-08 10:49:08 +0700

    Update Polymorfisme.js

--- Polymorfisme.js
+++ Polymorfisme.js
@@ -1,1 +1,63 @@
+// Definisi kelas induk
 
+class Orang {
+
+
+
+    tampilkanPeran() {
+
+        console.log("Peran : Orang biasa");
+
+    }
+
+}
+
+// Definisi kelas turunan
+
+class Mahasiswa extends Orang {
+
+    tampilkanPeran() {
+
+        console.log("Peran : Mahasiswa");
+
+    }
+
+}
+
+class Dosen extends Orang {
+
+    tampilkanPeran() {
+
+        console.log("Peran : Dosen");
+
+    }
+
+}
+
+class Tukangcilok extends Orang{
+
+    tampilkanPeran(){
+
+        console.log("Peran : Tukang Cilok")
+
+    }
+
+}
+
+// Menggunakan polimorfisme
+
+const orang1 = new Orang();
+
+const mahasiswa1 = new Mahasiswa();
+
+const dosen1 = new Dosen();
+
+const tukang1 = new Tukangcilok
+
+
+
+orang1.tampilkanPeran();    // Output: Peran: Orang biasa
+
+mahasiswa1.tampilkanPeran(); // Output: Peran: Mahasiswa
+
+dosen1.tampilkanPeran();     //
